825caa11b35e18775d45e1fe015656ee0cd560d1
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-09 11:07:04 +0530

    list

---
+++ Day3/lengthoflist.py
@@ -0,0 +1,4 @@
+list=[1,2,"hello",7.5,5,9]
+print(list)
+for i in range(len(list)):
+    print(list[i],end=" ")
---
+++ Day3/list.py
@@ -0,0 +1,5 @@
+list=[1,2,"hello",7.5,5,9]
+print(list[2])
+
+list2=[2,4,8,[6,9,7],"Hello",2.001]
+print(list2[3][2])
