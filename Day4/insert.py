8640db691defdcf64119beec21f65add9d1d8856
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-16 10:26:48 +0530

    Slice

---
+++ Day4/insert.py
@@ -0,0 +1,8 @@
+lst=[7,-4,-2,9,0,11,17,8,19,10]
+lst.append("Hello") #to add element in list
+# lst.insert(-4,36) #to insert elements in list
+lst1=[3,5,6]
+lst.extend(lst1) #Extend list from lst to lst1
+
+
+print(lst)
---
+++ Day4/sumofelement.py
@@ -0,0 +1,3 @@
+lst=[5,3,2,1,4,8]
+sum=lst[1]+lst[2]+lst[3]+lst[4]+lst[5]
+print(sum)
