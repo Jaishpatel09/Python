f9d5832922186f1da7424bd25c2ad15b5e44914b
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-21 09:48:32 +0530

    List

---
+++ Day4/function.py
@@ -0,0 +1,6 @@
+# list=[1,3,5,12,14,15,23]
+# del list[4]
+# print(list)
+list=[1,3,5,12,14,15,23]
+list.remove(3)
+print(list)
---
+++ Day4/rev.py
@@ -0,0 +1,4 @@
+list=[1,3,5,12,14,15,3,23]
+list1=sorted(list,reverse=True)
+print(list)
+print(list1)
