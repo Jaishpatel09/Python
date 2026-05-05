063c4dd83124f451e6146bfdcb21c6cb5221e00b
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-05 10:30:39 +0530

    Basucs Dictionary

---
+++ Day9/DictionaryBasics.py
@@ -0,0 +1,19 @@
+# data = { "name": "Jake", "age": 22 }
+# print(data)
+
+#Dictionary can store any type of data
+# thisdict = {    
+#   "brand": "Ford",
+#   "model": "Mustang",
+  #"year": 1964, #duplicate are not allowed
+  # "year": 2020
+# }
+# print(thisdict)
+# print(thisdict["brand"]) #print through the keys.
+# print(thisdict["model"]) #print only model from dictionary
+# print(len(thisdict)) #Length of Dictionary
+
+# print(type(thisdict))
+# to make a dictionary dict(.....)
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
