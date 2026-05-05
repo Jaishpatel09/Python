2c0942db9d5a2d481c3493f2cdfe71128b21ef8f
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-05 10:48:07 +0530

    Add Items in Dictionary

---
+++ Day9 Dictionary/AccessItems.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# x = thisdict["model"] #Get Value by the model key
+# # x=thisdict.get("model") #This is another mothod
+
+x=thisdict.keys() #It will return all keys of Dictionary
+print(x)
